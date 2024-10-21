c751a2fadc9c2da6b763d42292c3903f0a3db876
Mercanuis <brogramn@gmail.com>
2024-10-21 19:15:03 +0900

    ref: change use of strings in format

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -1,24 +1,25 @@
 extends Node
 
-var FILE_PATH = "res://lang/en.json"
+const FILE_PATH: String = "res://lang/en.json"
+
 
 func load_file() -> void:
-	var json = JSON.new()
-	var file = FileAccess.open(FILE_PATH, FileAccess.READ)
-	var json_text = file.get_as_text()
+	var json: JSON = JSON.new()
+	var file: FileAccess = FileAccess.open(FILE_PATH, FileAccess.READ)
+	var json_text: String = file.get_as_text()
 	file.close()
 	
 	var error = json.parse(json_text)
 	if error == OK:
-		var data = json.data
+		var data: Variant = json.data
 		if typeof(data) == TYPE_DICTIONARY:
 			print(data)
-			print("testing keys")
+			print("Testing keys\n")
 			var key_1: String = data["MY_KEY"]
-			print("Key 1: " + key_1)
-			print("Key 2: " + data["MY_BAD_KEY"])
+			print("Key {0}: {1}\n".format(["1", key_1]))
+			print("Key {0}: {1}\n".format(["2", data["MY_BAD_KEY"]]))
 		else:
-			print("data was of different type")
+			print("[data] was of different type than [TYPE_DICTIONARY]")
 
 func _ready():
 	load_file()
