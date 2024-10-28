96f1e58fe6a3815041d2678e999625db447ec985
Mercanuis <brogramn@gmail.com>
2024-10-28 18:36:59 +0900

    Ref: Make Dialog Script a Utility Class

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -1,8 +1,23 @@
+class_name DialogUtility
 extends Node
+
 
 const FILE_PATH: String = "res://lang/en.json"
 
-func load_file() -> Variant: 
+
+var text_dictionary
+
+
+func _ready():
+	var text_dictionary = _load_file()
+	if text_dictionary != null:	
+		for key in text_dictionary:
+			print("Key {0}: {1}\n".format([key, text_dictionary[key]]))
+	else:
+		print("[dialog:_ready]: unable to load dictionary")
+
+
+func _load_file() -> Variant: 
 	var json: JSON = JSON.new()
 	var file: FileAccess = FileAccess.open(FILE_PATH, FileAccess.READ)
 	var json_text: String = file.get_as_text()
@@ -20,11 +35,14 @@ func load_file() -> Variant:
 		print("[dialog:load_file]: unable to parse JSON")
 		return null
 
+func get_dialog(key: String) -> String:
+	return text_dictionary[key]
 
-func _ready():
-	var text_dictionary = load_file()
-	if text_dictionary != null:	
-		for key in text_dictionary:
-			print("Key {0}: {1}\n".format([key, text_dictionary[key]]))
-	else:
-		print("[dialog:_ready]: unable to load dictionary")
+	#dialog_box.show(text_dictionary["ENTRY_POINT"])
+	#await next.button.pressed
+	#dialog_box.show(text_dictionary["DECISION_POINT"])
+	#await decision_made
+	#dialog_box.show(text_dictionary["DECISION_POINT_CHOICE_1_RESPONSE"])
+	#await next.button.pressed
+	#dialog_box.show(text_dictionary["END_POINT"])
+
