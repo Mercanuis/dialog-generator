5f697a8fe9ef57653b79313e6184d6e70bb694f7
Mercanuis <brogramn@gmail.com>
2024-10-28 18:04:19 +0900

    Ref: cleanup

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -13,11 +13,6 @@ func load_file() -> Variant:
 		var data: Variant = json.data
 		if typeof(data) == TYPE_DICTIONARY:
 			return data
-			#print(data)
-			#print("Testing keys\n")
-			#var key_1: String = data["MY_KEY"]
-			#print("Key {0}: {1}\n".format(["1", key_1]))
-			#print("Key {0}: {1}\n".format(["2", data["MY_BAD_KEY"]]))
 		else:
 			print("[data] was of different type than [TYPE_DICTIONARY]")
 			return null
