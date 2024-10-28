1c10da8ad295c0e0580fb06f1876eb88e9d7751e
Mercanuis <brogramn@gmail.com>
2024-10-28 17:59:59 +0900

    Ref: dialog.gd for loading file separately from main

--- scripts/dialog.gd
+++ scripts/dialog.gd
@@ -2,8 +2,7 @@ extends Node
 
 const FILE_PATH: String = "res://lang/en.json"
 
-
-func load_file() -> void:
+func load_file() -> Variant: 
 	var json: JSON = JSON.new()
 	var file: FileAccess = FileAccess.open(FILE_PATH, FileAccess.READ)
 	var json_text: String = file.get_as_text()
@@ -13,13 +12,20 @@ func load_file() -> void:
 	if error == OK:
 		var data: Variant = json.data
 		if typeof(data) == TYPE_DICTIONARY:
-			print(data)
-			print("Testing keys\n")
-			var key_1: String = data["MY_KEY"]
-			print("Key {0}: {1}\n".format(["1", key_1]))
-			print("Key {0}: {1}\n".format(["2", data["MY_BAD_KEY"]]))
+			return data
+			#print(data)
+			#print("Testing keys\n")
+			#var key_1: String = data["MY_KEY"]
+			#print("Key {0}: {1}\n".format(["1", key_1]))
+			#print("Key {0}: {1}\n".format(["2", data["MY_BAD_KEY"]]))
 		else:
 			print("[data] was of different type than [TYPE_DICTIONARY]")
+			return null
+	else:
+		return null
 
 func _ready():
-	load_file()
+	var text_dictionary = load_file()
+	for key in text_dictionary:
+		print("Key {0}: {1}\n".format([key, text_dictionary[key]]))
+	
