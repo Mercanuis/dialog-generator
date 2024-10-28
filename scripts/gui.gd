d6356211dcdc053ca2450d7617883637a8c81417
Mercanuis <brogramn@gmail.com>
2024-10-28 19:46:57 +0900

    Ref: reorder calls

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -11,8 +11,8 @@ func _init() -> void:
 func _ready() -> void:
 	var start_dialog = _dialog_utils.get_dialog("ENTRY_POINT")
 	$HBCDialogBox/LabelTextWall.set_text(start_dialog)
-	print("[GUI._ready] initialized successfully")
 	$LabelPlaceholder.hide()
+	print("[GUI._ready] initialized successfully")
 
 
 func _process(_delta: float) -> void:
